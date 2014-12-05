b6aeffcf22f88e8f86614c4fe4f575da6e52e7d8
rickytux <rick.dsida@icloud.com>
2014-12-05 11:30:05 -0600

    Basic Bomb explodes
    Made sure form clears once the form is “submitted”.  Changed status of
    bomb to Detonated once the incorrect code has been entered three times.
    Still need to implement a countdown so the bomb once activated goes
    off because at the moment it doesn’t go off… ever

--- overlord/public/js/application.js
+++ overlord/public/js/application.js
@@ -1,28 +1,48 @@
 var intruder = 0
 $(window).load(function() {
+    var obj = JSON.parse(localStorage.yourObject || '{"Status": ""}')
+    document.getElementById('status').innerHTML = obj.Status
     $('form').on('submit', function(event) {
         event.preventDefault();
         var test = $('#message').val()
+        document.getElementById("form").reset();
         if(test == '1234') {
-            var result = "Activated"
-            document.getElementById('status').innerHTML = 'Activated'
-            var time = new Date().getTime();
-            console.log(time)
+            if ($('#status').text() == "Inactive"){
+                var r  = confirm("Activate the bomb?");
+                if (r==true) {
+                    document.getElementById('status').innerHTML = "Activated"
+                    obj.Status = "Activated";
+                    localStorage.yourObject = JSON.stringify(obj);
+                    var time = new Date().getTime();
+                    console.log(time)
+                } else {
+                    alert('Launch sequence aborted')
+
+                }
+            } else {
+                document.getElementById('status').innerHTML = "Inactive"
+                obj.Status = "Inactivate";
+                localStorage.yourObject = JSON.stringify(obj);
+
+            }
         } else {
             intruder++
-            console.log(intruder)
+            document.getElementById("form").reset();
             switch(intruder) {
+                case 1:
+                    $(this).append("<br>Possible intruder detected")
+                break;
                 case 2:
                     $(this).append("<br>Activating defensive measures")
                 break;
-                case 3:
-                    $(this).append("<br>Self Destruct imminent")
-                break;
                 default:
-                    $(this).append("<br>Possible intruder detected")
+                    $('#form').hide();
+                    $(this).parent().append("<br>Self Destruct imminent")
+                    document.getElementById('status').innerHTML = "Detonated"
+                    obj.Status = "Inactive";
+                    localStorage.yourObject = JSON.stringify(obj);
             }
         }
 
-        alert(result);
     });
 });
